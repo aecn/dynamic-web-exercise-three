4f106f2ca1e72e77f0749640ff8103fb89421796
annie <ac8502@nyu.edu>
2022-10-04 23:06:51 -0400

    weather data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { WEATHER_APP_API_KEY } from '../API_KEYS';
+// URL for accessing OpenWeatherAPI with Query Params
+const openWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=Orlando&appid=${WEATHER_APP_API_KEY}`;
 
 function Home() {
+    // Value stored in state for weather data
+    const [weatherData, setWeatherData] = useState({});
+ 
+useEffect(() => {
+// Query OpenWeatherAPI for weather data
+// Make request to OpenWeather based on city
+    axios
+    .get(openWeatherURL)
+    .then(function(response) {
+        setWeatherData(response.data);
+    })
+    .catch(function(error) {
+        console.warn(error);
+        setWeatherData({});
+    });
+}, []);
+
+    console.log("state value", weatherData);
+
     return (
         <div>
             <h1>Weather App</h1>
-            <p>{WEATHER_APP_API_KEY}</p>
+            <h2>{weatherData.name}</h2>
+            <p>Humidity: {weatherData.main && weatherData.main.humidity}%</p>
         </div>
     );
 }
